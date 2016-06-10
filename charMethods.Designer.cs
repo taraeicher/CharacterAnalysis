cde6b17551482d9222afdf54a2b25e0fc39422e5
taraeicher <taraeicher@yahoo.com>
2016-06-10 18:59:19 -0500

    Add files via upload

---
+++ charMethods.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CharMethods
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.txtInputChar = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAnalyze.Location = new System.Drawing.Point(29, 85);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(90, 43);
+            this.btnAnalyze.TabIndex = 1;
+            this.btnAnalyze.Text = "&Analyze Character";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInput.Location = new System.Drawing.Point(34, 37);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(134, 19);
+            this.lblInput.TabIndex = 1;
+            this.lblInput.Text = "Enter a character:";
+            // 
+            // txtInputChar
+            // 
+            this.txtInputChar.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInputChar.Location = new System.Drawing.Point(209, 29);
+            this.txtInputChar.Name = "txtInputChar";
+            this.txtInputChar.Size = new System.Drawing.Size(100, 27);
+            this.txtInputChar.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(217, 85);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(90, 43);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(29, 162);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(280, 219);
+            this.txtOutput.TabIndex = 4;
+            this.txtOutput.TabStop = false;
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(123, 85);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(90, 43);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmMain
+            // 
+            this.AcceptButton = this.btnAnalyze;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(341, 407);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtInputChar);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.btnAnalyze);
+            this.Name = "frmMain";
+            this.Text = "Static Character Methods";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.TextBox txtInputChar;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ charMethods.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CharMethods
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtOutput.Text = AnalyzeCharacter(char.Parse(txtInputChar.Text));
+                txtInputChar.SelectAll();
+                txtInputChar.Focus();
+            }
+            catch (FormatException)
+            {
+
+                MessageBox.Show("You may supply only a single character.", "Input Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtInputChar.SelectAll();
+                txtInputChar.Focus();
+            }
+        }
+
+        private string AnalyzeCharacter(char chInput)
+        {
+            string strOutput;
+
+            strOutput = "is digit: " +
+               Char.IsDigit(chInput) + "\r\n";
+
+            strOutput += "is letter: " +
+               Char.IsLetter(chInput) + "\r\n";
+
+            strOutput += "is letter or digit: " +
+               Char.IsLetterOrDigit(chInput) + "\r\n";
+
+            strOutput += "is lower case: " +
+               Char.IsLower(chInput) + "\r\n";
+
+            strOutput += "is upper case: " +
+               Char.IsUpper(chInput) + "\r\n";
+
+            strOutput += "to upper case: " +
+               Char.ToUpper(chInput) + "\r\n";
+
+            strOutput += "to lower case: " +
+               Char.ToLower(chInput) + "\r\n";
+
+            strOutput += "is punctuation: " +
+               Char.IsPunctuation(chInput) + "\r\n";
+
+            strOutput += "is symbol: " + Char.IsSymbol(chInput);
+
+            return strOutput;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtOutput.Clear();
+            txtInputChar.Clear();
+            txtInputChar.Focus();
+        }
+    }
+}
